fd7bbe490fa6a33b787574108f61178c312182ab
immrachel <rachel940629@gmail.com>
2024-08-24 01:42:32 +0900

    feat: styled components getInitialProps 추가

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,14 +1,48 @@
-import {Html, Head, Main, NextScript, DocumentContext} from "next/document";
-import {ServerStyleSheet} from "styled-components";
+import Document, {
+  DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head></Head>
+        <body>
+          <Main />
+          <NextScript />
+          <div id="modal" />
+        </body>
+      </Html>
+    );
+  }
 }
+
+MyDocument.getInitialProps = async (ctx: DocumentContext): Promise<any> => {
+  const sheet = new ServerStyleSheet();
+  const originalRenderPage = ctx.renderPage;
+
+  try {
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App: any) => (props) =>
+          sheet.collectStyles(<App {...props} />),
+      });
+    const initialProps = await Document.getInitialProps(ctx);
+    return {
+      ...initialProps,
+      styles: (
+        <>
+          {initialProps.styles}
+          {sheet.getStyleElement()}
+        </>
+      ),
+    };
+  } finally {
+    sheet.seal();
+  }
+};
