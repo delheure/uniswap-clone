4d312d0b5aa8d9f09d042b3198752664d51551f8
immrachel <rachel940629@gmail.com>
2024-08-24 16:55:41 +0900

    style: search input 스타일 파일로 분리

---
+++ src/common/header/search-input/style.ts
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+
+export const SearchInputWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 4px;
+  min-width: 280px;
+  max-width: 400px;
+  width: 100vw;
+  height: 40px;
+  padding: 8px 16px;
+  background-color: rgb(249, 249, 249);
+  border: 1px solid rgba(34, 34, 34, 0.07);
+  border-radius: 20px;
+`;
+
+export const SearchInputBox = styled.input`
+  width: 100%;
+  color: var(--text-default);
+  background: transparent;
+  border: 0;
+  outline: 0;
+
+  &::placeholder {
+    color: #7d7d7d;
+  }
+`;
+
+export const NFTSearchBox = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 0px 8px;
+  width: 20px;
+  height: 20px;
+  color: rgb(125, 125, 125);
+  font-size: 12px;
+  font-weight: 535;
+  line-height: 16px;
+  border-radius: 4px;
+  background-color: rgba(34, 34, 34, 0.07);
+  backdrop-filter: blur(60px);
+  opacity: 0.6;
+`;
