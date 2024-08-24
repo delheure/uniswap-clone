6b616a6efa841784268aac66d4ebcbe0da076992
immrachel <rachel940629@gmail.com>
2024-08-24 15:51:58 +0900

    feat: 불필요한 파일 정리

--- src/pages/swap/index.tsx
+++ src/pages/swap/index.tsx
@@ -1,19 +1,15 @@
 import React, { useState } from "react";
 import SwapTab from "src/common/swap-tab";
 import SwapContents from "~/pages/swap/src/feature/swap-contents";
-import useModal from "~/common/modal/useModal";
 import { ButtonBox, SwapWrapper, TabField } from "~/pages/swap/style";
 import SvgIcon from "~/common/svg-icon";
 import SettingIcon from "~/assets/ico/icon_setting.svg";
 
 const SwapPage = () => {
-  const { openModal } = useModal();
   const [selectedTab, setSelectedTab] = useState("swap");
 
   const renderContent = () => {
     switch (selectedTab) {
-      case "limit":
-        return <>no</>;
       case "swap":
       default:
         return <SwapContents />;
